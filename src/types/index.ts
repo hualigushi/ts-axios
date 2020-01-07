ae5b15612e4727864daee0ce1ecd610a387c6bb4
money <hualigushi@163.com>
2020-01-07 16:39:36 +0800

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,6 +17,7 @@ export interface AxiosRequestConfig {
     tranformRequest?: AxiosTransformer | AxiosTransformer[]
     transformResponse?: AxiosTransformer | AxiosTransformer[]
     cancelToken?: CancelToken
+    withCredentials?: boolean
   
     [propName: string]: any // 索引签名
 }
