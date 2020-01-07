f652723551198928795a447b29b281f47236b457
money <hualigushi@163.com>
2020-01-07 11:22:44 +0800

    合并策略

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -1,4 +1,5 @@
-import { isPlainObject } from './util'
+import { isPlainObject, deepMerge } from './util'
+import { Method } from '../types'
 
 // 规范化content-type 为 Content-Type
 function normalizeHeaderName (headers: any, normalizeName: string): void {
@@ -43,4 +44,20 @@ export function parseHeaders (headers: string): any {
     })
 
     return parsed
+}
+
+export function flattenHeaders (headers: any, method: Method): any {
+    if (!headers) {
+        return headers
+        }
+  
+  headers = deepMerge(headers.common, headers[method], headers)
+  
+  const methodsToDelete = ['delete','get', 'head', 'options', 'post', 'put', 'patch', ' common']
+  
+  methodsToDelete.forEach(method => {
+      delete headers[method]
+  })
+  
+  return headers
 }
