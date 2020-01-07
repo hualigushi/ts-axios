bac1841170423aba4bbd24e126eb5502f9b92f98
money <hualigushi@163.com>
2020-01-07 13:53:30 +0800

    请求和响应配置化

--- src/default.ts
+++ src/default.ts
@@ -1,4 +1,6 @@
 import { AxiosRequestConfig } from './types'
+import { processHeaders } from './helpers/headers'
+import { transformRequest, tranformResponse } from './healpers/data'
 
 const defaults : AxiosRequestConfig = {
     method: 'get',
@@ -8,6 +10,19 @@ const defaults : AxiosRequestConfig = {
             Accept: 'application/json, text/plain, */*'
         }
     }
+  
+  transformRequest: [
+     function (data: any, headers? : any): any {
+        processHeaders(data, headers)
+        return transformRequest(data)
+     }
+  ],
+  
+  transformResponse: [
+     function (data: any):any {
+        return tranformResponse(data)
+     }
+  ]
 }
 
 const methodsNoData = ['delete', 'get', 'head', 'options']
