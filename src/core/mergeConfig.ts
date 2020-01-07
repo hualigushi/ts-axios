05d671238c60e2ba62ae5f649065a124c0e0343c
money <hualigushi@163.com>
2020-01-07 11:00:30 +0800

    配置

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -12,11 +12,30 @@ function fromVal2Strat (val1: any, val2: any): any {
     }
 }
 
+function deepMergeStrat (val1: any, val2: any): any {
+    if (isPlainObject(val2)) {
+        return deepMerge(val2, val2)
+        } else if (typeof val2 !== 'undefined') {
+               return val2
+        } else if (isPlainObejct(val1)) {
+          return deepMerge(val1)
+                   }else if (typeof val1 !== 'undefined') {
+               return val1
+        }
+}
+
 const stratKeysFromVal2 = ['url', 'params', 'data']
 
 stratKeysFromVal2.forEach(key => {
     strats[key] = fromVal2Strat
 })
+
+const stratKeysDeepMerge = ['headers']
+
+stratKeysDeepMerge.forEach(key => {
+    strats[key] = deepMergeStrat
+})
+
 
 export default function mergeConfig (config1: AxiosRequestConfig, config2?: AxiosRequestConfig): AxiosRequestConfig {
    if (!config2) {
@@ -28,9 +47,18 @@ export default function mergeConfig (config1: AxiosRequestConfig, config2?: Axio
    for (let key in config2) {
       mergeField(key)
    }
+  
+  for (let key in config1) {
+    if (!config2[key]) {
+        mergeField(key)
+        }
+      
+   }
    
    function mergeFiled (key: string): void {
        const strat = strats[key] || defaultStrat
-       config[key] = strat(config1[key], config2[key])
+       config[key] = strat(config1[key], config2![key])
    }
+  
+  return config
 }
