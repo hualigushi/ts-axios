204a923f638d37b5ae72577155f11b07796fda95
money <hualigushi@163.com>
2020-01-07 16:30:00 +0800

    Update dispatchRequest.ts

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -5,6 +5,7 @@ import { flattenHeaders } from '../helpers/headers'
 import { transform } from './transform'
 
 export default function despatchRequest(config: AxiosRequestConfig): AxiosPromise {
+  throwIfCancellationRequested(config)
     processConfig(config) // 处理config
     return xhr(config).then((res) => {
         return transformResponseData(res)
@@ -24,4 +25,10 @@ function transformURL(config: AxiosRequestConfig): string {
 function transformResponseData(res: AxiosResponse): AxiosResponse {
     res.data = transform(res.data, res.headers, res.config.transfromResponse)
     return res
+}
+
+function throwIfCancellationRequested(config: AxiosRequestConfig): void {
+   if (config.cancelToken) {
+     config.cancelToken.throwIfRequested()
+       }
 }
