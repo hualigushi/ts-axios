ea1d5d47f2f38c824e20c8b9ef4ff6a3dc54c9d2
money <hualigushi@163.com>
2020-01-14 07:39:29 +0000

    Update request.spec.ts

--- test/request.spec.ts
+++ test/request.spec.ts
@@ -238,4 +238,36 @@ describe('requests', () => {
               })
            })
        })
+
+     test('should support array buffer response', done => {
+         let response: AxiosResponse
+
+         function str2ab(string: string) {
+             const buff = new ArrayBuffer(str.length * 2)
+             const view = new Uint16Array(buff)
+             for(let i = 0; i < str.length; i++) {
+               view[i] = str.charCodeAt(i)
+            }
+            return buff
+         }
+
+        axios('/foo', {
+           responseType: 'arraybuffer'
+        }).then(data => {
+            response = data
+        })
+
+        getAjaxRequest().then(request => {
+            request.respondWith({
+                status: 200,
+                // @ts-ignore
+                response: str2ab('hello world')
+            })
+
+            setTimeout(() => {
+               expect(response.data.byteLength).toBe(22)
+               done()
+            }, 100)
+        })
+     })
 })
